08a35f8a49c525f1dafcf170a9415f6146cd35d9
nabaafatima <nabaafatima7097@gmail.com>
2026-06-05 17:46:40 +0500

    hover effect for the hollow page

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const ninjagoLink = document.getElementById("ninjago");
 const ninjagoImg = document.getElementById("e");
 const boy = document.querySelector(".boyimg");
-
+const hollowimg = document.getElementById("a");
+const hollowlink = document.getElementById("hollow");
 ninjagoLink.addEventListener("mouseenter", () => {
     ninjagoImg.style.opacity = "1";
     boy.style.opacity = "0";
 });
 ninjagoLink.addEventListener("mouseleave", () => {
     ninjagoImg.style.opacity = "0";
+    boy.style.opacity = "1";
+});
+hollowlink.addEventListener("mouseenter", ()=> {
+    hollowimg.style.opacity = "1";
+    boy.style.opacity = "0";
+});
+hollowlink.addEventListener("mouseleave", () =>{
+    hollowimg.style.opacity = "0";
     boy.style.opacity = "1";
 });
